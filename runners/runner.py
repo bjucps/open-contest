a949d6c0bedfa9d15137cbd3de8243be47bd5933
thecavemanlogic <cevan828@students.bju.edu>
2021-03-08 15:14:05 -0500

    Simplified code and changed back to original directory

--- runners/runner.py
+++ runners/runner.py
@@ -1,25 +1,22 @@
 import os
 
-# return that amount of time the child process spent in user mode
+# Return that amount of CPU time the child process
+# spent in user mode and system mode in seconds
 def total_child_execution_time():
-    f = open(f"/proc/{os.getpid()}/stat")
-    data = f.read().split(" ")
-    f.close()
+    with open(f"/proc/{os.getpid()}/stat") as f:
+        data = f.read().split(" ")
 
-    # return (cutime + cstime) / 100
+    # convert sum into seconds and return
     return (float(data[15]) + float(data[16])) / 100
 
 def runCode(testCases: int, timeLimit: int, compile: str, run: str) -> int:
 
     os.chdir("/source")
-
-    # print("Compiling...")
-
-    # os.system("echo /source/out: $(ls /source)")
     
     # Compile submission
     if os.system(f"{compile} > out/compile_out.txt 2> out/compile_error.txt") != 0:
         print("compile_error")
+        os.chdir("..")
         return 1
     
     # Get the child CPU time after compile is done so that 
@@ -35,6 +32,7 @@ def runCode(testCases: int, timeLimit: int, compile: str, run: str) -> int:
         currentTime = total_child_execution_time() - start_time
         if currentTime > timeLimit:
             print("TLE")
+            os.chdir("..")
             return 1
         
         # Check exit status of submission
@@ -42,4 +40,6 @@ def runCode(testCases: int, timeLimit: int, compile: str, run: str) -> int:
             f.write("ok" if status == 0 else "runtime_error")
     
     print("ok")
+    os.chdir("..")
+
     return 0
