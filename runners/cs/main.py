83d3c0dad0ba863c0938e36d7005a997cb3f9cfa
thecavemanlogic <cevan828@students.bju.edu>
2021-03-08 14:27:42 -0500

    Simplified runners

--- runners/cs/main.py
+++ runners/cs/main.py
@@ -1,31 +1,9 @@
-from threading import Timer, Thread
-import time
-import os
 import sys
+import runner
 
-def kill():
-    print("TLE")
-    exit(1)
-
-testCases = int(sys.argv[1])
-timeLimit = int(sys.argv[2])
-
-def runCode(timeout):
-    if os.system("csc /source/code.cs -out:/source/code.exe 2> /source/out/compile_out.txt > /source/out/compile_error.txt") != 0:
-        print("compile_error")
-        timeout.cancel()
-        exit(1)
-    for i in range(testCases):
-        result = "ok"
-        if os.system("mono /source/code.exe < /source/in{0}.txt > /source/out/out{0}.txt 2> /source/out/err{0}.txt".format(i)) != 0:
-            result = "runtime_error"
-        with open("/source/out/result{0}.txt".format(i), "w") as f:
-            f.write(result)
-    print("ok")
-    timeout.cancel()
-    exit(0)
-
-timeout = Timer(timeLimit, kill)
-timeout.start()
-thread = Thread(target=runCode, args=(timeout,), daemon=True)
-thread.start()
+runner.runCode(
+    int(sys.argv[1]),
+    int(sys.argv[2]),
+    "csc code.cs -out:code.exe",
+    "./code"
+)
