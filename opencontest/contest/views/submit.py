69abd922c29d39b005827086f0e5f22ab9b5ad9f
Stephen Schaub <sschaub@bju.edu>
2021-03-13 10:08:18 -0500

    Force judge review of submissions where all sample tests were correct if samples break ties

--- opencontest/contest/views/submit.py
+++ opencontest/contest/views/submit.py
@@ -16,6 +16,7 @@ from django.http import JsonResponse
 from contest.auth import logged_in_required, admin_required
 from contest.models.problem import Problem
 from contest.models.submission import Submission
+from contest.models.contest import Contest
 from contest.models.user import User
 
 from opencontest.settings import OC_MAX_DISPLAY_LEN, OC_MAX_OUTPUT_LEN, OC_DOCKERIMAGE_BASE
@@ -165,6 +166,7 @@ def runCode(sub: Submission, user: User) -> list:
             results = []
             result = "ok"
 
+            all_samples_correct = True
             for i in range(numTests):
                 if sub.type == Submission.TYPE_CUSTOM:
                     inputs.append(sub.custominput)
@@ -200,8 +202,16 @@ def runCode(sub: Submission, user: User) -> list:
                 if res != "ok" and result == "ok":
                     result = res
 
+                if i < prob.samples and res != "ok":
+                    all_samples_correct = False
+
+            may_autojudge = True
             sub.result = result
-            if sub.result in ["ok", "runtime_error", "tle"] or user.isAdmin():
+            if Contest.getCurrent():
+                if Contest.getCurrent().tieBreaker and all_samples_correct and sub.result in ["runtime_error", "tle"]:
+                    # Force review of submissions where all sample tests were correct if samples break ties
+                    may_autojudge = False
+            if sub.result == "ok" or (may_autojudge and sub.result in ["runtime_error", "tle"]) or user.isAdmin():
                 sub.status = Submission.STATUS_JUDGED
                 
             sub.results = results
