ae01a742fffef3eb6be9d614b99906c9fdf02626
Stephen Schaub <sschaub@bju.edu>
2021-03-13 12:12:27 -0500

    Move newrelic config to db folder

--- opencontest/startup.sh
+++ opencontest/startup.sh
@@ -3,11 +3,16 @@
 # This script runs in the Docker container and starts the Django application
 
 STARTUP_CMD="uwsgi --threads 50 --http 0.0.0.0:8000 --module opencontest.wsgi --ini uwsgi.ini $*"
-if [ "$OC_MONITORING" -eq 1 ]; then
-    echo "Starting CMS application in Docker with newrelic monitoring: $STARTUP_CMD"
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program $STARTUP_CMD
-else
-    echo "Starting CMS application in Docker: $STARTUP_CMD"
-    $STARTUP_CMD
+
+if [ "$OC_MONITORING" = 1 ]; then
+    if [ -r /db/newrelic.ini ]; then
+        echo -e "Starting CMS application in Docker with New Relic monitoring ...\n$STARTUP_CMD"
+        NEW_RELIC_CONFIG_FILE=/db/newrelic.ini newrelic-admin run-program $STARTUP_CMD
+    else
+        echo "** New Relic monitoring enabled but newrelic.ini not found in database folder, starting without New Relic"
+    fi
 fi
+echo -e "Starting CMS application in Docker... \n$STARTUP_CMD"
+$STARTUP_CMD
+
 
