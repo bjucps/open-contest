b423a7d78d6df67c2532a7435c9582fb3630732c
Stephen Schaub <sschaub@bju.edu>
2023-03-11 09:56:44 -0500

    Update

--- util/install.sh
+++ util/install.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+USER=cms
+
 apt install -y docker.io jq
 
-# Create ubuntu user
-useradd -m -s /bin/bash ubuntu
+# Create user
+useradd -m -s /bin/bash $USER
 
-# Add ubuntu to groups
-usermod -a -G docker,adm,sudo ubuntu
+# Add $USER to groups
+usermod -a -G docker,adm,sudo $USER
 
 # Install software
-cd /home/ubuntu
+cd /home/$USER
 git clone https://github.com/bjucps/open-contest
 cd open-contest
 docker build -t bjucps/open-contest opencontest
 cd runners
 bash build.sh
-cd /home/ubuntu
+cd /home/$USER
 cp -r open-contest/test/db .
 
-# Copy .ssh authorized keys to ubuntu user
-cp -r ~/.ssh /home/ubuntu
-chown -R ubuntu:ubuntu * .ssh
+# Copy .ssh authorized keys to $USER user
+cp -r ~/.ssh /home/$USER
+chown -R $USER:$USER * .ssh
 
 # Register system services
 cp open-contest/*.service /etc/systemd/system
