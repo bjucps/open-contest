8acd23be932a12e9b194f028db527bc6733daf43
Stephen Schaub <sschaub@bju.edu>
2023-10-28 21:54:10 -0400

    Update to Python 3.11

--- runners/python3/main.py
+++ runners/python3/main.py
@@ -4,6 +4,6 @@ import runner
 runner.runCode(
     int(sys.argv[1]),
     int(sys.argv[2]),
-    "python3.8 -m py_compile code.py",
-    "python3.8 code.py"
+    "python3 -m py_compile code.py",
+    "python3 code.py"
 )
